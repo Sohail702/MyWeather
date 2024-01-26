cdd864a59e09722f77fd89df26f9fc6c4aaa4b9c
Sohail702 <118183667+Sohail702@users.noreply.github.com>
2024-01-26 15:02:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+const apiKey = "0bdbe0f04d079d6dc2078c50087dcdfb";
+
+const cityElement = document.querySelector(".city");
+const temperature = document.querySelector(".temp");
+const windSpeed = document.querySelector(".wind-speed");
+const humidity = document.querySelector(".humidity");
+const visibility = document.querySelector(".visibility-distance");
+
+const descriptionText = document.querySelector(".description-text");
+const descriptionIcon = document.querySelector(".description i");
+
+
+async function fetchWeatherData(city) {
+    try {
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+        );
+
+        if (!response.ok) {
+            throw new Error("Unable to fetch weather data");
+        }
+        const data = await response.json();
+        console.log(data);
+      
+        updateWeatherUI(data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+// fetchWeatherData();
+
+function updateWeatherUI(data) {
+    cityElement.textContent = data.name;
+    temperature.textContent = `Temperature : ${Math.round(data.main.temp)}°C`;
+    windSpeed.textContent = `${data.wind.speed} km/h`;
+    humidity.textContent = `${data.main.humidity}%`;
+    visibility.textContent = `${data.visibility / 1000} km`;
+    descriptionText.textContent = data.weather[0].description;
+
+    const currentDate = new Date();
+    date.textContent = currentDate.toDateString();
+    const weatherIconName = getWeatherIconName(data.weather[0].main);
+    descriptionIcon.innerHTML = `<i class="material-icons">${weatherIconName}</i>`;
+}
+
+const formElement = document.querySelector(".search-form");
+const inputElement = document.querySelector(".city-input");
+
+formElement.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const city = inputElement.value;
+    if (city !== "") {
+        fetchWeatherData(city);
+        inputElement.value = "";
+    }
+});
+
+function getWeatherIconName(weatherCondition) {
+    const iconMap = {
+        Clear: "wb_sunny",
+        Clouds: "wb_cloudy",
+        Rain: "umbrella",
+        Thunderstorm: "flash_on",
+        Drizzle: "grain",
+        Snow: "ac_unit",
+        Mist: "cloud",
+        Smoke: "cloud",
+        Haze: "cloud",
+        Fog: "cloud",
+    };
+
+    return iconMap[weatherCondition] || "help";
+}
+
+function updateTodayDate() {
+    const dateParagraph = document.querySelector(".date");
+
+    if (dateParagraph) {
+        const today = new Date();
+        const options = { day: 'numeric', month: 'short', year: 'numeric' };
+        const formattedDate = today.toLocaleDateString('en-US', options);
+        
+        dateParagraph.textContent = `Date : ${formattedDate}`;
+    }
+}
+
+updateTodayDate();
+
+
+
+
+
+   
